eadd53b8e6268f3622edfe662b1d4a9d0ab801de
Chong Shen Lua <shenlua1@gmail.com>
2023-07-31 14:25:04 -0500

    vercel_analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Noto_Sans } from "@next/font/google";
 
 const noto_sans = Noto_Sans({
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={noto_sans.className}>
       <Component {...pageProps} />;
+      <Analytics />
     </main>
   );
 }
